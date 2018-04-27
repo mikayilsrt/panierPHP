1adfa888c2618650b591182f8e8dab79b5df3c37
Mikayil SERT <sert.mikayil@gmail.com>
2018-04-28 00:28:01 +0200

    Change of the class Database

--- App/Config/Database.php
+++ App/Config/Database.php
@@ -35,7 +35,7 @@ class Database {
      */
     public function getPDO () {
         if ($this->pdo === null) {
-            $pdo = new PDO("mysql:host=localhost;dbname=e-commerce", "root", "", array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+            $pdo = new PDO("mysql:host=$this->db_host;dbname=$this->db_name", $this->db_user, $this->db_password, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->pdo = $pdo;
         }
